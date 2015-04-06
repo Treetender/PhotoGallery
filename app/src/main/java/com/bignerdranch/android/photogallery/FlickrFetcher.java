eb1cbc3b96f20e63b1430a734b5b2307880b423c
treetender <treetender@noreply.github.com>
2015-04-06 00:00:45 -0300

    Learning the art of REST APIs in Flickr and Asynchronous tasks!!

--- app/src/main/java/com/bignerdranch/android/photogallery/FlickrFetcher.java
+++ app/src/main/java/com/bignerdranch/android/photogallery/FlickrFetcher.java
@@ -1,5 +1,8 @@
 package com.bignerdranch.android.photogallery;
 
+import android.net.Uri;
+import android.util.Log;
+
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -10,6 +13,14 @@ import java.net.URL;
  * Created by treetender on 4/5/15.
  */
 public class FlickrFetcher {
+    public static final String TAG = "FlickrFetcher";
+
+    private static final String API_KEY = "558d77f708dd3a379603984bf599623a";
+    private static final String ENDPOINT = "http://api.flickr.com/services/rest/";
+    private static final String METHOD_GET_RECENT = "flickr.photos.getRecent";
+    private static final String PARAM_EXTRAS = "extras";
+    private static final String EXTRA_SMALL_URL = "url_s";
+
     byte[] getUrlBytes(String urlspec) throws IOException {
         URL url = new URL(urlspec);
         HttpURLConnection connection = (HttpURLConnection)url.openConnection();
@@ -37,5 +48,20 @@ public class FlickrFetcher {
 
     public String getUrl(String urlspec) throws IOException {
         return new String(getUrlBytes(urlspec));
+    }
+
+    public void fetchItems() {
+        try {
+            String url = Uri.parse(ENDPOINT).buildUpon()
+                            .appendQueryParameter("method", METHOD_GET_RECENT)
+                            .appendQueryParameter("api_key", API_KEY)
+                            .appendQueryParameter(PARAM_EXTRAS, EXTRA_SMALL_URL)
+                            .build().toString();
+            String xmlString = getUrl(url);
+            Log.i(TAG, "Received xml: " + xmlString);
+        }
+        catch (IOException ioe) {
+            Log.e(TAG, "Failed to fetch items", ioe);
+        }
     }
 }
--- app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryFragment.java
+++ app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryFragment.java
@@ -1,23 +1,37 @@
 package com.bignerdranch.android.photogallery;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.GridView;
 
+import java.io.IOException;
+
 /**
  * Created by treetender on 4/5/15.
  */
 public class PhotoGalleryFragment extends Fragment {
+    private static final String TAG = "PhotoGalleryFragment";
     GridView mGridView;
+
+    private class FetchItemsTask extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected Void doInBackground(Void... params) {
+            new FlickrFetcher().fetchItems();
+            return null;
+        }
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setRetainInstance(true);
+        new FetchItemsTask().execute();
     }
 
     @Override
